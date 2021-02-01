45fbda4fd176449f097635c6304ad3af600ea615
edmarques11 <edmarquessantosdelima@gmail.com>
2021-02-01 11:45:29 -0300

    alteraçoes para deploy

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,17 @@
 import firebase from 'firebase/app'
+import 'firebase/analytics'
+
 import 'firebase/auth'
-import 'firebase/storage'
 import 'firebase/firestore'
-import 'firebase/database'
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATADASE_URL,
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 }
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
